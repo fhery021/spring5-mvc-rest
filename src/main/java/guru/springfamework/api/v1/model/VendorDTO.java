f974b1f37973c87ac2ee1b8679ff3a0a3f809f41
Ferenc Bondar <fhery021@gmail.com>
2019-06-24 15:26:15 +0300

    Vendor swagger documentation assignment done

--- src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
@@ -1,6 +1,7 @@
 package guru.springfamework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VendorDTO {
+
+    @ApiModelProperty(value = "Name of the vendor", required = true)
     private String name;
 
+    @ApiModelProperty(value = "URL of the vendor, generated automatically based on vendor id.")
     @JsonProperty("vendor_url")
     private String vendorUrl;
 
